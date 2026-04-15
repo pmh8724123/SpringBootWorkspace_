af568c635fd5bbe3a104de6c66bbd913b39c27db
DESKTOP-IPL4B0T\user1 <pmh8724123@gmail.com>
2026-04-15 17:24:51 +0900

    SpringBoot commit

---
+++ src/main/java/com/kh/menu/MenuApplication.java
@@ -0,0 +1,24 @@
+package com.kh.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/* 
+ * @SpringBootApplication
+ *  - 여러 어노테이션을 합쳐놓은 스프링부트 프로젝트에 한개만 작성해야 하는 어노테이션
+ *  1. SpringBootconfiguration
+ *   - springboot설정파일임을 의미하는 어노테이션
+ *  2. EnableAutoConfiguration
+ *   - 자동설정 활성화
+ *  3. ComponentScan
+ *   - 현재실행된 클래스를 기준으로 "하위패키지"를 모두 검사하여 bean객체로 등록해주는 어노테이션
+ * */
+
+@SpringBootApplication
+public class MenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuApplication.class, args);
+	}
+
+}
